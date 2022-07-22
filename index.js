0c77a7bf365a4de645fe364a9c18537b2667b1e8
Fly Gor <447268514@qq.com>
2022-07-22 17:05:08 +0800

    新增用户

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes").auth;
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ mongoose
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/user", authRoute);
 
 // 监听端口
 app.listen(8080, () => {
